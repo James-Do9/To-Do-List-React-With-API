60c821ac420d5f58c72fdac7e44fd96f711e8041
James-Do9 <dojames3@yahoo.com>
2020-12-21 14:40:14 +0000

    Update

--- src/js/component/ToDoList.js
+++ src/js/component/ToDoList.js
@@ -8,28 +8,72 @@ export class ToDoList extends React.Component {
 			task: "",
 			taskCounter: 0
 		};
+		this.url = "https://assets.breatheco.de/apis/fake/todos/user/dojam";
+	}
+	componentDidMount() {
+		fetch(this.url)
+			.then(function(response) {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then(jsonifiedResponse =>
+				this.setState({ taskList: jsonifiedResponse })
+			)
+			.catch(function(error) {
+				console.log("Looks like there was a problem: \n", error);
+			});
 	}
 	addList = e => {
 		if (e.keyCode == 13) {
-			let updatedTaskList = this.state.taskList; //Better to change the state through a local variable instead of doing it directly
-			updatedTaskList.push(this.state.task);
-			this.setState({ taskList: updatedTaskList }); //Updates the taskList array with the newly inputed task by the user
 			let updatedCounter = this.state.taskCounter;
 			updatedCounter++;
 			this.setState({ taskCounter: updatedCounter }); //Increments the counter by +1 every time the user enters a task, the counter number is displayed
 			//on the bottom of the lists
+
 			let resetTask = this.state.task;
 			resetTask = "";
 			this.setState({ task: resetTask }); //Resets the task on the input bar so the user can freely type a new task without having to backspace or delete
+
+			if (this.state.task) {
+				var newListObject = { label: this.state.task, done: false };
+				var updatedList = this.state.taskList.concat(newListObject);
+				this.setState({ taskList: updatedList });
+				fetch(this.url, {
+					method: "PUT", // or 'POST'
+					body: JSON.stringify(updatedList), // data can be `string` or {object}!
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(res => res.json())
+					.then(response =>
+						console.log("Success:", JSON.stringify(response))
+					)
+					.catch(error => console.error("Error:", error));
+			}
 		}
 	};
 	removeList = index => {
 		let updatedTaskList = this.state.taskList;
 		updatedTaskList.splice(index, 1);
 		this.setState({ taskList: updatedTaskList });
+
 		let updatedCounter = this.state.taskCounter;
 		updatedCounter--;
 		this.setState({ taskCounter: updatedCounter });
+
+		fetch(this.url, {
+			method: "PUT", // or 'POST'
+			body: JSON.stringify(updatedTaskList), // data can be `string` or {object}!
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(res => res.json())
+			.then(response => console.log("Success:", JSON.stringify(response)))
+			.catch(error => console.error("Error:", error));
 	};
 	render() {
 		return (
@@ -56,12 +100,10 @@ export class ToDoList extends React.Component {
 								<li
 									className="list-group-item listText"
 									key={index}>
-									{content}{" "}
+									{content.label}{" "}
 									<span
 										className="float-right"
-										onClick={index =>
-											this.removeList(index)
-										}>
+										onClick={() => this.removeList(index)}>
 										<i
 											className="fas fa-times"
 											style={{ color: "red " }}
